3f9e991caad32a31301517583343ab5fd9338fc4
Lucas <lucas14y@gmail.com>
2021-08-20 03:13:41 -0300

    fix all

--- src/main/java/com/lucassants/voteapi/DTO/ErrorDTO.java
+++ src/main/java/com/lucassants/voteapi/DTO/ErrorDTO.java
@@ -1,12 +1,21 @@
 package com.lucassants.voteapi.DTO;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.lucassants.voteapi.views.ScheduleView;
 
+@JsonSerialize
 public class ErrorDTO {
+    @JsonView(ScheduleView.class)
     @JsonProperty("name")
     private String name;
+    @JsonView(ScheduleView.class)
     @JsonProperty("description")
     private String description;
+
+    public ErrorDTO() {
+    }
 
     public ErrorDTO(String name, String description) {
         this.name = name;
